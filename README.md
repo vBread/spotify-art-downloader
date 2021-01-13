020a4f6a93242fb9afa6fae2a15f52677172e048
Oscar Lee-Vermeren <thevortexspirit@gmail.com>
2021-01-13 13:38:02 -0500

    refactor: general cleanup

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
 # Spotify Art Downloader
 
 Requirements
-- [Node.js](https://nodejs.org/)
+
+-   [Node.js](https://nodejs.org/)
 
 ## Installation
-```
+
+```sh
 $ git clone https://github.com/vBread/spotify-art-downloader.git
 $ cd spotify-art-downloader
 $ npm install
 ```
 
 ## Usage
-```diff
-$ npm start -- <url> [directory] 
-# A default directory will be made inside if one isn't specified
+
+```sh
+$ npm start -- <url> [directory]
+# A default directory called "Covers" will be made in this folder if one isn't specified
 
 # Examples
 $ npm start -- https://open.spotify.com/track/61MbTEmCqOxcpF9Z1QBhcK?si=Zv7TP3_BTGWut0HQ5cXcrw
 $ npm start -- https://open.spotify.com/track/1UURGn40bicGsMi19bqml0?si=Zsmwf8IfR0uZRvWmobj2Ug ~/Downloads
 
-# NOTE: For Mac users using zsh, the url will need to be double quoted
+# For Mac users using zsh, the url will need to be double quoted
 $ npm start -- "https://open.spotify.com/track/2DZqu0ju4GPs4nJXm1XrQ1?si=ZvmWL8PySjOm9xBJH24nCA"
 ```
 
 ## Setup
+
 1. Go to the [Spotify Developer Dashboard](https://developer.spotify.com/dashboard/login) page and login
 
 <img src="https://imgur.com/SatR1rH.png"></img>
@@ -38,8 +42,9 @@ $ npm start -- "https://open.spotify.com/track/2DZqu0ju4GPs4nJXm1XrQ1?si=ZvmWL8P
 
 <img src="https://imgur.com/8M4WoUQ.png"></img>
 
-4. Open the `.env` file and fill in the respective fields
+1. Open the `.env.example` file, fill in the respective fields, and rename it to `.env`
+
 ```js
-CLIENT_ID=xxxxx
-CLIENT_SECRET=xxxxx
+CLIENT_ID = xxxxx;
+CLIENT_SECRET = xxxxx;
 ```
--- index.ts
+++ index.ts
@@ -1,100 +1,96 @@
-import fetch from 'node-fetch'
-import { join, resolve } from 'path'
-import { mkdir, writeFile } from 'fs/promises'
-import { parse, URLSearchParams } from 'url'
+import fetch from 'node-fetch';
+import { join, resolve } from 'path';
+import { mkdir, writeFile } from 'fs/promises';
+import { parse, URLSearchParams } from 'url';
 
-const dir = process.argv[3]
+const dir = process.argv[3];
 
 interface Token {
-    access_token: string
+	access_token: string;
 }
 
 interface Track {
-    album: {
-        images: { url: string }[]
-    },
-    artists: { name: string }[]
-    name: string
+	album: {
+		images: { url: string }[];
+	};
+	artists: { name: string }[];
+	name: string;
 }
 
-class CoverDownload {
-    private readonly id: string
-    private readonly secret: string
-    private data = new URLSearchParams()
-    private file: string
+void new (class CoverDownload {
+	private file: string;
 
-    public constructor(id: string, secret: string) {
-        this.data.append('grant_type', 'client_credentials')
+	public constructor(private readonly id: string, private readonly secret: string) {
+		if (!id || !secret) {
+			throw new Error('Missing credentials');
+		}
 
-        if (!id || !secret) {
-            throw new Error('Missing credentials')
-        }
+		this.download();
+	}
 
-        this.id = id
-        this.secret = secret
-        this.download()
-    }
+	private async download(): Promise<void> {
+		const track: Track = await (
+			await fetch(this.url, {
+				headers: {
+					Authorization: `Bearer ${await this.token()}`,
+				},
+			})
+		).json();
 
-    private async download(): Promise<void> {
-        const res: Track = await (await fetch(this.url, {
-            headers: {
-                'Authorization': `Bearer ${(await this.token()).access_token}`
-            }
-        })).json()
+		const filename = `${track.artists[0].name} | ${track.name}.jpg`;
 
-        const name = `${res.artists[0].name} | ${res.name}.jpg`
+		if (dir) {
+			this.file = join(dir, filename);
+		} else {
+			this.file = resolve(await this.default(), filename);
+		}
 
-        if (dir) {
-            this.file = join(dir, name)
-        } else {
-            this.file = resolve(await this.default(), name)
-        }
+		writeFile(
+			this.file,
+			await (
+				await fetch(track.album.images[0].url, {
+					method: 'GET',
+				})
+			).buffer()
+		);
 
-        const data = await (await fetch(res.album.images[0].url, {
-            method: 'GET'
-        })).buffer()
+		console.log(`Saved to ${this.file}`);
+	}
 
-        writeFile(this.file, data)
-        console.log(`Saved to ${this.file}`)
-    }
+	private async token(): Promise<Token> {
+		const auth = Buffer.from(`${this.id}:${this.secret}`).toString('base64');
 
-    private async token(): Promise<Token> {
-        const auth = Buffer.from(`${this.id}:${this.secret}`).toString('base64')
+		const res = await fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			headers: {
+				Authorization: `Basic ${auth}`,
+			},
+			body: new URLSearchParams([['grant_type', 'client_credentials']]),
+		});
 
-        const res = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Basic ${auth}`
-            },
-            body: this.data
-        })
+		if (res.ok) {
+			return (await res.json()).access_token;
+		}
 
-        if (res.ok) {
-            return await res.json()
-        }
+		throw new Error(`[${res.status}]: ${res.statusText}`);
+	}
 
-        throw new Error(`[${res.status}]: ${res.statusText}`)
-    }
+	private get url(): string {
+		const parsed = parse(process.argv[2]);
 
-    private get url(): string {
-        const parsed = parse(process.argv[2])
+		if (/https?/.test(parsed.protocol)) {
+			const [type, id] = parsed.path.split('/');
 
-        if (/https?/.test(parsed.protocol)) {
-            const type = parsed.path.split('/')[1]
-            const songID = parsed.path.split('/')[2]
+			return `https://api.spotify.com/v1/${type}s/${id}`;
+		}
 
-            return `https://api.spotify.com/v1/${type}s/${songID}`
-        }
+		throw new Error('Failed to build url');
+	}
 
-        throw new Error('Failed to build url')
-    }
+	private async default(): Promise<string> {
+		const dir = `${process.cwd()}/Covers`;
+		await mkdir(dir).catch(() => {});
 
-    private async default(): Promise<string> {
-        const dir = `${process.cwd()}/Covers` 
-        await mkdir(dir).catch(() => {})
-
-        return dir
-    }
-}
-
-new CoverDownload(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
+		return dir;
+	}
+})(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
